0fab14d8df9f714c2ea146a31a2a53d35d375997
Anjana-T-A <89142107+Anjana-T-A@users.noreply.github.com>
2025-03-16 14:16:49 +0000

    Update App.tsx

--- ethics-frontend/src/App.tsx
+++ ethics-frontend/src/App.tsx
@@ -1,27 +1,26 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom"; // Correct import
-import './App.css';
-import SupervisorDashboard from './Pages/supervisor_dashboard'; // Ensure the path is correct
-import Dashboard from './Pages/dashboard';
-import EthicsReviewerDashboard from './Pages/reviewer_dashboard';
-import SupervisorEthics from './Pages/supervisor_ethics';
-import LoginPage from './Pages/login';
+import { Routes, Route, Navigate } from "react-router-dom"; 
+import "./App.css";
+import SupervisorDashboard from "./Pages/supervisor_dashboard";
+import Dashboard from "./Pages/dashboard";
+import EthicsReviewerDashboard from "./Pages/reviewer_dashboard";
+import SupervisorEthics from "./Pages/supervisor_ethics";
+import LoginPage from "./Pages/login";
 
 const App: React.FC = () => {
     const [message, setMessage] = useState<string>("Loading...");
 
     return (
-        <Router>
-            <main className="main-content">
-                <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/ethics_reviewer" element={<EthicsReviewerDashboard />} />
-                    <Route path="/supervisor" element={<SupervisorDashboard />} />
-                    <Route path="/supervisor_ethics" element={<SupervisorEthics />} />
-                    <Route path="/login" element={<LoginPage />} />
-                </Routes>
-            </main>
-        </Router>
+        <main className="main-content">
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/ethics_reviewer" element={<EthicsReviewerDashboard />} />
+                <Route path="/supervisor" element={<SupervisorDashboard />} />
+                <Route path="/supervisor_ethics" element={<SupervisorEthics />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </main>
     );
 };
 
